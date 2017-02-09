039bc7316ea4646b385a85430f4d49abfa8e7d78
Joe L <jlistro@gmail.com>
2017-02-09 12:59:20 -0500

    Update PlaceListFragment.java
    Add a place picker

--- app/src/main/java/com/upmoon/alexanderbean/barcrawlr/fragments/PlaceListFragment.java
+++ app/src/main/java/com/upmoon/alexanderbean/barcrawlr/fragments/PlaceListFragment.java
@@ -1,20 +1,33 @@
 package com.upmoon.alexanderbean.barcrawlr.fragments;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlacePicker;
 import com.upmoon.alexanderbean.barcrawlr.R;
+
+import static android.app.Activity.RESULT_OK;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class PlaceListFragment extends Fragment {
+    private static final int PLACE_PICKER_REQUEST = 2;
 
-
+    /*
+    *   Update: added place picker button
+    *   TODO: add places list to aggregate all places for the plan
+    */
     public PlaceListFragment() {
         // Required empty public constructor
     }
@@ -25,6 +38,46 @@ public class PlaceListFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_place_list, container, false);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        final Button addPlaceButton = (Button) getActivity().findViewById(R.id.add_place_button);
+        addPlaceButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                onPickButtonClick(v);
+            }
+        });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == PLACE_PICKER_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                Place place = PlacePicker.getPlace(data, getActivity());
+                String toastMsg = String.format("Place: %s", place.getName());
+                Toast.makeText(getActivity(), toastMsg, Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    public void onPickButtonClick(View v) {
+        // Construct an intent for the place picker
+        try {
+            PlacePicker.IntentBuilder intentBuilder =
+                    new PlacePicker.IntentBuilder();
+            Intent intent = intentBuilder.build(getActivity());
+            // Start the intent by requesting a result,
+            // identified by a request code.
+            startActivityForResult(intent, PLACE_PICKER_REQUEST);
+
+        } catch (GooglePlayServicesRepairableException e) {
+            // ...
+        } catch (GooglePlayServicesNotAvailableException e) {
+            // ...
+        }
     }
 
 }
