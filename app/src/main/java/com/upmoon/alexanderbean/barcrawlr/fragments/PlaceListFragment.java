58083f5636249d44d4df45f3106ce6795567af51
Joe Listro <jlistro@gmail.com>
2017-02-10 19:51:14 -0500

    Update PlaceListFragment.java

--- app/src/main/java/com/upmoon/alexanderbean/barcrawlr/fragments/PlaceListFragment.java
+++ app/src/main/java/com/upmoon/alexanderbean/barcrawlr/fragments/PlaceListFragment.java
@@ -22,13 +22,52 @@ import static android.app.Activity.RESULT_OK;
  * A simple {@link Fragment} subclass.
  */
 public class PlaceListFragment extends Fragment {
-    private static final int PLACE_PICKER_REQUEST = 1;
-
-    /*
-    *   Update: added place picker button
-    *   TODO: add places list to aggregate all places for the plan
-    */
     public PlaceListFragment() {
         // Required empty public constructor
     }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_place_list, container, false);
+
+        final Button addPlaceButton = (Button) v.findViewById(R.id.add_place_button);
+        addPlaceButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                onPickButtonClick(v);
+            }
+        });
+
+        return v;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == PLACE_PICKER_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                Place place = PlacePicker.getPlace(getActivity(),data);
+                String toastMsg = String.format("Place: %s", place.getName());
+                Toast.makeText(getActivity(), toastMsg, Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    public void onPickButtonClick(View v) {
+        // Construct an intent for the place picker
+        try {
+            PlacePicker.IntentBuilder intentBuilder =
+                    new PlacePicker.IntentBuilder();
+            Intent intent = intentBuilder.build(getActivity());
+            // Start the intent by requesting a result,
+            // identified by a request code.
+            startActivityForResult(intent, PLACE_PICKER_REQUEST);
+
+        } catch (GooglePlayServicesRepairableException e) {
+            Toast.makeText(getActivity(), "Google Play Services Repairable Exception", Toast.LENGTH_LONG).show();
+        } catch (GooglePlayServicesNotAvailableException e) {
+            Toast.makeText(getActivity(), "Google Play Services Not Available", Toast.LENGTH_LONG).show();
+        }
+    }
+
 }
