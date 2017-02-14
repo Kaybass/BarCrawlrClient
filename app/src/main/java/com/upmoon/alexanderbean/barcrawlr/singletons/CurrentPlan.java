befb454592ba0b6790c1885ee89776aa6bf6bed1
joeListro <jlistro@gmail.com>
2017-02-14 16:29:46 -0500

    Update CurrentPlan singleton object so it now holds a Plan object.

--- app/src/main/java/com/upmoon/alexanderbean/barcrawlr/singletons/CurrentPlan.java
+++ app/src/main/java/com/upmoon/alexanderbean/barcrawlr/singletons/CurrentPlan.java
@@ -1,15 +1,24 @@
 package com.upmoon.alexanderbean.barcrawlr.singletons;
 
+import com.upmoon.alexanderbean.barcrawlr.model.Place;
+import com.upmoon.alexanderbean.barcrawlr.model.Plan;
+
 /**
  * Created by AlexanderBean on 1/23/2017.
  */
 public class CurrentPlan {
     private static CurrentPlan ourInstance = new CurrentPlan();
+    public Plan plan;
 
     public static CurrentPlan getInstance() {
         return ourInstance;
     }
 
     private CurrentPlan() {
+    }
+
+    public void addPlaceToPlan(Place place) {
+        plan.addPlace(place);
+        return;
     }
 }
