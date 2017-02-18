d5561c1d17c3fa12c1c3a9d156a2067de93b4102
Alexander Bean Apmann <caboose200@gmail.com>
2017-02-18 17:14:37 -0500

    Added tasks for the UI

--- app/src/main/java/com/upmoon/alexanderbean/barcrawlr/networking/Connector.java
+++ app/src/main/java/com/upmoon/alexanderbean/barcrawlr/networking/Connector.java
@@ -17,11 +17,56 @@ import com.upmoon.alexanderbean.barcrawlr.model.User;
  *  - notify the server if user is going home
  */
 public interface Connector {
+
+    /**
+     * sendPlan
+     * @param plan
+     * @param self
+     * @return
+     *
+     * In the network simulator sendPlan will check
+     * the validity of the plan it is given, in the
+     * real world this will be done by the server.
+     * If the plan is loosely valid this method will
+     * return a json object that contains the status
+     * of the plan and the list of current users (
+     * which will only contain the user who sent the
+     * plan)
+     */
     String sendPlan(Plan plan, User self);
 
-    String sendCode(String code);
+    /**
+     * sendCode
+     * @param code
+     * @param self
+     * @return
+     *
+     * sendCode in the real world will send a code
+     * to the server and if the code is real the server
+     * will send status + user list like in sendPlan
+     */
+    String sendCode(String code, User self);
 
-    String locationUpdate(User self);
+    /**
+     * locationUpdate
+     * @param code
+     * @param self
+     * @return
+     *
+     * locationUpdate sends the user's location info to
+     * the server and in turn the server sends the status
+     * of that action back + the information of other users
+     */
+    String locationUpdate(String code, User self);
 
-    String disconnect();
+    /**
+     * disconnect
+     * @param code
+     * @param self
+     * @return
+     *
+     * disconnect tells the server that the user
+     * wishes to stop using the app.
+     */
+    String disconnect(String code, User self);
 }
