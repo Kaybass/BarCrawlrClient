fe68cf21ee62c45791fa0bf6b31fa8f34877ee7a
Joe Listro <jlistro@gmail.com>
2017-02-20 20:10:26 -0500

    PlaceListFragment now functions properly :)

--- app/src/main/java/com/upmoon/alexanderbean/barcrawlr/fragments/PlanCreator/PlaceListFragment.java
+++ app/src/main/java/com/upmoon/alexanderbean/barcrawlr/fragments/PlanCreator/PlaceListFragment.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -32,17 +33,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static android.app.Activity.RESULT_OK;
+import static android.content.Context.LAYOUT_INFLATER_SERVICE;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class PlaceListFragment extends Fragment {
     private static final int PLACE_PICKER_REQUEST = 1;
-    //private int PLACES_EMPTY = 0;
 
     // Define variables for the places' ListView and its corresponding ArrayAdapter.
     private RecyclerView mPlacesRecyclerView;
-    PlaceExpandableAdapter mPlaceExpandableAdapter;
+    private PlaceAdapter mPlaceAdapter;
 
     public PlaceListFragment() {
         // Required empty public constructor
@@ -54,14 +55,11 @@ public class PlaceListFragment extends Fragment {
         // Inflate the layout for this fragment.
         View v = inflater.inflate(R.layout.fragment_place_list, container, false);
 
-        mPlaceExpandableAdapter = new PlaceExpandableAdapter(getActivity(), generatePlacesList());
-        mPlaceExpandableAdapter.setCustomParentAnimationViewId(R.id.parent_list_item_expand_arrow);
-        mPlaceExpandableAdapter.setParentClickableViewAnimationDefaultDuration();
-        mPlaceExpandableAdapter.setParentAndIconExpandOnClick(true);
-
-        mPlacesRecyclerView = (RecyclerView) getActivity().findViewById(R.id.places_recycler_view);
-        mPlacesRecyclerView.setAdapter(mPlaceExpandableAdapter);
-
+        mPlaceAdapter = new PlaceAdapter();
+        mPlacesRecyclerView = (RecyclerView) v.findViewById(R.id.places_recycler_view);
+        mPlacesRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity(),
+                                                LinearLayoutManager.VERTICAL,false));
+        mPlacesRecyclerView.setAdapter(mPlaceAdapter);
 
         // Initialize the addPlaceButton.
         final Button addPlaceButton = (Button) v.findViewById(R.id.add_place_button);
@@ -96,7 +94,7 @@ public class PlaceListFragment extends Fragment {
                 // Add ourPlace (new place) to current plan.
                 CurrentPlan.getInstance().addPlaceToPlan(ourPlace);
 
-                mPlaceExpandableAdapter.notifyDataSetChanged();
+                mPlaceAdapter.notifyDataSetChanged();
 
             }
         }
@@ -119,119 +117,56 @@ public class PlaceListFragment extends Fragment {
         }
     }
 
-/*    public ArrayList<String> getPlacesNames() {
-        ArrayList<String> placeNames = new ArrayList<>();
-        for (int i = 0; i < CurrentPlan.getInstance().getNumPlaces(); i++) {
-            placeNames.add(CurrentPlan.getInstance().getPlace(i).getName());
-        }
-
-        return placeNames;
-    }*/
-
-    /**
-     * loadPlaces()
-     *  - The purpose of this function is to create and set the RecyclerView.Adapter for places_recycler_view
-     *     with the Places from the CurrentPlan.
-     */
-    public void loadPlaces() {
-    }
-
-    private ArrayList<ParentObject> generatePlacesList() {
-        ArrayList<ParentObject> parentObjects = new ArrayList<>();
-        ArrayList<com.upmoon.alexanderbean.barcrawlr.model.Place> places = CurrentPlan.getInstance().getPlaces();
-        for (com.upmoon.alexanderbean.barcrawlr.model.Place place : places) {
-            ArrayList<Object> childList = new ArrayList<>();
-            childList.add(new PlaceChildObject(place.getAddress()));
-            place.setChildObjectList(childList);
-            parentObjects.add(place);
-        }
-
-        return parentObjects;
-    }
     // Begin : Accessory classes used in the RecyclerView
 
     /**
      * The purpose of this class is to be a custom RecyclerView.Adapter that will expand when a user presses the expand_arrow.
      */
-    private class PlaceExpandableAdapter extends ExpandableRecyclerAdapter<PlaceParentViewHolder, PlaceChildViewHolder> {
 
-        LayoutInflater mInflater;
+    private class PlaceHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
-        public PlaceExpandableAdapter(Context context, List<ParentObject> parentList) {
-            super(context, parentList);
+        private TextView placeName, placeAddress;
 
-            mInflater = LayoutInflater.from(context);
+        public PlaceHolder(View itemView) {
+            super(itemView);
+            itemView.setOnClickListener(this);
+
+            placeName = (TextView) itemView.findViewById(R.id.place_holder_name);
+
+            placeAddress = (TextView) itemView.findViewById(R.id.place_holder_address);
+        }
+
+        public void bindPlace(int pos) {
+            placeName.setText(CurrentPlan.getInstance().getPlace(pos).getName());
+
+            placeAddress.setText(CurrentPlan.getInstance().getPlace(pos).getAddress());
         }
 
         @Override
-        public PlaceParentViewHolder onCreateParentViewHolder(ViewGroup viewGroup) {
-            View v = mInflater.inflate(R.layout.list_item_place_parent, viewGroup, false);
-            return new PlaceParentViewHolder(v);
-        }
+        public void onClick(View v) {
 
-        @Override
-        public PlaceChildViewHolder onCreateChildViewHolder(ViewGroup viewGroup) {
-            View v = mInflater.inflate(R.layout.list_item_place_child, viewGroup, false);
-            return new PlaceChildViewHolder(v);
-        }
-
-        @Override
-        public void onBindParentViewHolder(PlaceParentViewHolder placeParentViewHolder, int i, Object parentObject) {
-            com.upmoon.alexanderbean.barcrawlr.model.Place place = (com.upmoon.alexanderbean.barcrawlr.model.Place) parentObject;
-            placeParentViewHolder.mPlaceNameTextView.setText(place.getName());
-        }
-
-        @Override
-        public void onBindChildViewHolder(PlaceChildViewHolder placeChildViewHolder, int i, Object childObject) {
-            PlaceChildObject placeChildObject = (PlaceChildObject) childObject;
-            placeChildViewHolder.mPlaceAddressTextView.setText(placeChildObject.getAddress());
-            placeChildViewHolder.mPlaceDeleteButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Toast.makeText(v.getContext(), "TODO : Call a delete place function here", Toast.LENGTH_SHORT);
-                    }
-                });
-            }
-
-            @Override
-            public int getItemCount(){ return CurrentPlan.getInstance().getNumPlaces(); }
-        }
-
-        private class PlaceParentViewHolder extends ParentViewHolder {
-            TextView mPlaceNameTextView;
-            ImageButton mParentDropDownArrow;
-
-            PlaceParentViewHolder(View itemView) {
-                super(itemView);
-
-                mPlaceNameTextView = (TextView) itemView.findViewById(R.id.parent_list_item_place_name_text_view);
-                mParentDropDownArrow = (ImageButton) itemView.findViewById(R.id.parent_list_item_expand_arrow);
-            }
-        }
-
-        private class PlaceChildViewHolder extends ChildViewHolder {
-            TextView mPlaceAddressTextView;
-            Button mPlaceDeleteButton;
-
-            PlaceChildViewHolder(View itemView) {
-                super(itemView);
-
-                mPlaceAddressTextView = (TextView) itemView.findViewById(R.id.child_list_item_place_address_text_view);
-                mPlaceDeleteButton = (Button) itemView.findViewById(R.id.child_list_item_delete_place_button);
-            }
-        }
-
-
-        private class PlaceChildObject {
-            private String mAddress;
-
-            public PlaceChildObject(String address) {
-                mAddress = address;
-            }
-
-            public String getAddress() {
-                return mAddress;
-            }
         }
     }
+
+    private class PlaceAdapter extends RecyclerView.Adapter<PlaceHolder> {
+        public PlaceAdapter() {
+
+        }
+
+        @Override
+        public PlaceHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            LayoutInflater LI = LayoutInflater.from(getActivity());
+            View view = LI.inflate(R.layout.place_holder,parent,false);
+            return new PlaceHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(PlaceHolder holder, int position) { holder.bindPlace(position); }
+
+        @Override
+        public int getItemCount() {
+            return CurrentPlan.getInstance().getNumPlaces();
+        }
+    }
+
 }
