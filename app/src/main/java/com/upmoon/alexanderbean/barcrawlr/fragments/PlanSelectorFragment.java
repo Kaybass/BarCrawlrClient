6a5468b732e1b7a6c11dce966d573ae03875c0a7
Joe Listro <jlistro@gmail.com>
2017-02-28 19:18:51 -0500

    Fixed dialog issue.

--- app/src/main/java/com/upmoon/alexanderbean/barcrawlr/fragments/PlanSelectorFragment.java
+++ app/src/main/java/com/upmoon/alexanderbean/barcrawlr/fragments/PlanSelectorFragment.java
@@ -287,30 +287,9 @@ public class PlanSelectorFragment extends Fragment {
         @Override
         public void onClick(View v){
 
-            final AlertDialog.Builder builder = new AlertDialog.Builder(getActivity())
-                    .setTitle("Are you sure you want to delete your plan?")
-                    .setMessage("This action cannot be undone.")
-                    .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface diag, int which) {
-                            // TODO: Delete the Plan
-                            PlanLoader pl = new PlanLoader(getActivity());
+            final AlertDialog.Builder buildo = new AlertDialog.Builder(getActivity());
 
-                            pl.deletePlan(planName.getText().toString());
-                        }
-                    })
-                    .setNegativeButton("No", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface diag, int which) {
-                            diag.cancel();
-                        }
-                    }).setCancelable(true);
-
-            final AlertDialog alertDialogConfirmation = builder.create();
-
-            final AlertDialog.Builder buildo = new AlertDialog.Builder(getActivity())
-                    .setTitle(planName.getText().toString())
-                    .setView(getView())
+            buildo.setTitle(planName.getText().toString())
                     .setPositiveButton("Edit Plan",new DialogInterface.OnClickListener(){
                         @Override
                         public void onClick(DialogInterface diag, int which){
@@ -328,7 +307,28 @@ public class PlanSelectorFragment extends Fragment {
                     .setNegativeButton("Delete",new DialogInterface.OnClickListener(){
                         @Override
                         public void onClick(DialogInterface diag, int which) {
-                            diag.dismiss();
+
+                            final AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+                            builder.setTitle("Are you sure you want to delete your plan?")
+                                    .setMessage("This action cannot be undone.")
+                                    .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface diag, int which) {
+                                            // TODO: Delete the Plan
+                                            PlanLoader pl = new PlanLoader(getActivity());
+
+                                            pl.deletePlan(planName.getText().toString());
+                                        }
+                                    })
+                                    .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface diag, int which) {
+                                            diag.cancel();
+                                        }
+                                    }).setCancelable(true);
+
+                            final AlertDialog alertDialogConfirmation = builder.create();
 
                             alertDialogConfirmation.show();
                         }
